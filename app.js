26c64c9fe0e07141a893432f9091bce5f5ac57f9
Ryan Fransen-Pruden <ryanfransenpruden@gmail.com>
2022-04-14 11:40:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+// loop over all buttons
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = 'green';
+        }
+        if (count < 0) {
+            value.style.color = 'red';
+        }
+        if (count === 0) {
+            value.style.color = '#222';
+        }
+        value.textContent = count;
+    });
+});
+
