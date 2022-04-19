d0ae77d712fcbfd2a676d194f7fe77ed29f6a13e
Ryan Fransen-Pruden <ryanfransenpruden@gmail.com>
2022-04-19 11:09:18 -0500

    upda

--- app.js
+++ app.js
@@ -6,25 +6,24 @@ const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 // loop over all buttons
 btns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        const styles = e.currentTarget.classList;
-        if (styles.contains("decrease")) {
-            count--;
-        } else if (styles.contains('increase')) {
-            count++;
-        } else {
-            count = 0;
-        }
-        if (count > 0) {
-            value.style.color = 'green';
-        }
-        if (count < 0) {
-            value.style.color = 'red';
-        }
-        if (count === 0) {
-            value.style.color = '#222';
-        }
-        value.textContent = count;
-    });
+  btn.addEventListener('click', function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = 'green';
+    }
+    if (count < 0) {
+      value.style.color = 'red';
+    }
+    if (count === 0) {
+      value.style.color = '#222';
+    }
+    value.textContent = count;
+  });
 });
-
